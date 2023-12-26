f55ece936ff9b20db12a45044f7ea049b9c9d6d4
abdullahriad-alghamdi <abdullahriad.alghamdi@gmail.com>
2023-12-26 09:31:45 +0300

    deleted unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import { useEffect } from 'react'
-import Router from './routes/Router'
 import { useDispatch } from 'react-redux'
 import { AppDispatch } from './redux/store'
+import Router from './routes/Router'
 
-import { fetchAllProducts, fetchProducts } from './redux/slices/products/productSlice'
 import { fetchCategories } from './redux/slices/categories/categorySlice'
 import { fetchOrders } from './redux/slices/orders/orderSlice'
+import { fetchAllProducts } from './redux/slices/products/productSlice'
 import { fetchUsers } from './redux/slices/usersList/userSlice'
 
-import { NavBar } from './components/pages/Home/NavBar'
-import Footer from './components/pages/Home/Footer'
 import { ToastContainer } from 'react-toastify'
+import Footer from './components/pages/Home/Footer'
+import { NavBar } from './components/pages/Home/NavBar'
 
 function App() {
   const dispatch: AppDispatch = useDispatch()
+
   useEffect(() => {
     dispatch(fetchAllProducts())
     dispatch(fetchCategories())
     dispatch(fetchOrders())
     dispatch(fetchUsers())
   }, [dispatch])
+
   return (
     <div className="app">
       <NavBar />
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
-import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-
+import { BrowserRouter } from 'react-router-dom'
 import { store } from './redux/store'
+
 import App from './App'
 
-import './style/Main.scss'
-import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'react-toastify/dist/ReactToastify.css'
+import './style/Main.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
