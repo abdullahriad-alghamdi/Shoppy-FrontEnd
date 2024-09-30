eecfd440a9198cb730bbec6b42fc1510c593da54
Abdullah Alghamdi <abdullahriad.alghamdi@gmail.com>
2024-09-30 14:12:44 +0300

    login information added in the side

--- src/pages/user/loginRegistration/Login.tsx
+++ src/pages/user/loginRegistration/Login.tsx
@@ -59,47 +59,60 @@ const Login = () => {
   }
 
   return (
-    <section className="login-registry-container">
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            value={user.email}
-            name="email"
-            onChange={handleChange}
-            required
-            autoComplete="off"
-          />
-        </Form.Group>
+    // section in the next to show the login info if anyone wants to use it
+    <section className="loginPage">
+      <section className="userInfo">
+        <h4>للتسجيل الدخول </h4> <strong>اسم المستخدم:</strong> <br />
+        <span className="user-name">admin1@gmail.com</span> <br /> <strong>كلمة المرور:</strong>{' '}
+        <br />
+        <span className="user-name">asdf4321</span> <br /> <br />
+        <strong> اسم المستخدم: </strong>
+        <br /> <span className="user-name">user@gmail.com</span> <br />
+        <strong> كلمة المرور: </strong>
+        <br /> <span className="user-name">asdf4321</span>
+      </section>
+      <section className="login-registry-container">
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={user.email}
+              name="email"
+              onChange={handleChange}
+              required
+              autoComplete="off"
+            />
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            value={user.password}
-            name="password"
-            onChange={handleChange}
-            required
-            autoComplete="on"
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Remember me" />
-        </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={user.password}
+              name="password"
+              onChange={handleChange}
+              required
+              autoComplete="on"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Remember me" />
+          </Form.Group>
 
-        <Button type="submit" variant="dark">
-          Login
-        </Button>
-        {/* forgot password form */}
-        <Form.Group className="mb-3 mt-3" controlId="formBasicCheckbox">
-          <Form.Text className="text-muted">
-            <Link to="/forgotPassword">Forgot password?</Link>
-          </Form.Text>
-        </Form.Group>
-      </Form>
+          <Button type="submit" variant="dark">
+            Login
+          </Button>
+          {/* forgot password form */}
+          <Form.Group className="mb-3 mt-3" controlId="formBasicCheckbox">
+            <Form.Text className="text-muted">
+              <Link to="/forgotPassword">Forgot password?</Link>
+            </Form.Text>
+          </Form.Group>
+        </Form>
+      </section>
     </section>
   )
 }
